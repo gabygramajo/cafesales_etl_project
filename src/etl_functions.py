a82cb768587286d4f5ba033d9991921218935431
Gabriel Gramajo <ggabrieldeveloper@gmail.com>
2026-02-04 13:28:58 +0000

    feat(etl): implement clean_data logic and refine ingestion docs
    - Added clean_data function with robust type casting and string normalization.\n- Updated ingest_data docstrings to Sphinx/reST format.\n- Applied Int64 and float64 conversions strictly.

--- src/etl_functions.py
+++ src/etl_functions.py
@@ -9,15 +9,13 @@ def ingest_data(file_path: str) -> Optional[pd.DataFrame]:
     """
     Ingesta datos desde un CSV, normaliza headers y maneja valores nulos estándar.
     
-    Args:
-        file_path: Ruta absoluta o relativa al archivo CSV.
-        
-    Returns:
-        pd.DataFrame: DataFrame con los datos crudos.
-        
-    Raises:
-        FileNotFoundError: Si el archivo no existe.
+    :param file_path: Ruta absoluta o relativa al archivo CSV.
+    :type file_path: str
+    :return: DataFrame con los datos crudos
+    :rtype: DataFrame | None
+    :raises FileNotFoundError: Si el archivo no existe.
     """
+    
     try:
         df = pd.read_csv(
             file_path, 
@@ -43,3 +41,48 @@ def ingest_data(file_path: str) -> Optional[pd.DataFrame]:
         raise
 
     return None
+
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Realiza limpieza exhaustiva: estandariza headers, fechas, strings y tipos numéricos.
+    
+    :param df: DataFrame con los datos crudos.
+    :type df: pd.DataFrame
+    :return: DataFrame con los datos limpios.
+    :rtype: DataFrame
+    """
+    
+    df_clean = df.copy()
+    
+    # normalizanción de headers (Snake Case)
+    df_clean.columns = df_clean.columns \
+                .str.strip() \
+                .str.lower() \
+                .str.replace(' ', '_')
+    
+    # normalizar fechas
+    df_clean["transaction_date"] = pd.to_datetime(
+        df_clean["transaction_date"], 
+        format='%Y-%m-%d', 
+        errors='coerce'
+    )
+  
+    # normalizar texto de columnas tipo str
+    text_cols = ["item", "payment_method", "location"]
+    
+    for col in text_cols:
+        # .str accessor respeta los NaNs automáticamente
+        df_clean[col] = df_clean[col].str.strip().str.title()
+    
+    # nomalizar columnas de enteros a Int64
+    df_clean["quantity"] = pd.to_numeric(df_clean["quantity"], errors='coerce').astype('Int64')
+    
+    # nomalizar columnas de flotantes a float64
+    money_cols = ["price_per_unit", "total_spent"]
+    
+    for col in money_cols:
+        df_clean[col] = pd.to_numeric(df_clean[col], errors='coerce').astype('float64')
+      
+    logging.info(f"Limpieza completada. Nulos en fechas: {df_clean['transaction_date'].isna().sum()}")
+    
+    return df_clean
